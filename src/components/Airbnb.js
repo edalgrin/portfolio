7a9fbece711a6b28d592f85e55de5986998da191
edalgrin <allegrini.eduardo@gmail.com>
2025-03-25 10:27:20 +0100

    Add missing sections and card

--- src/components/Airbnb.js
+++ src/components/Airbnb.js
@@ -1,6 +1,7 @@
 //"use client";
 //import { useState } from "react";
 //import classNames from "classnames";
+import classNames from "classnames";
 import {
     IoOptions,
     IoGlobeOutline,
@@ -8,6 +9,7 @@ import {
     IoSearchOutline,
     IoHomeOutline,
     IoStar,
+    IoHeartOutline
 } from "react-icons/io5";
 
 const navCategories = [
@@ -69,6 +71,108 @@ const navCategories = [
     }
 ]
 
+const cards = [
+    {
+        id: "1",
+        title: "San Martino in Badia, Italia",
+        rating: 5.0,
+        host: "Host: azienda specializzata",
+        price: 336,
+        date: "12 - 17 Apr",
+        images: [
+            "https://picsum.photos/id/76/300.jpg",
+            "https://picsum.photos/id/77/300.jpg",
+            "https://picsum.photos/id/78/300.jpg",
+        ],
+    },
+    {
+        id: "2",
+        title: "San Martino in Badia, Italia",
+        rating: 5.0,
+        host: "Host: azienda specializzata",
+        price: 336,
+        date: "12 - 17 Apr",
+        images: [
+            "https://picsum.photos/id/26/300.jpg",
+            "https://picsum.photos/id/27/300.jpg",
+            "https://picsum.photos/id/28/300.jpg",
+        ],
+    },
+    {
+        id: "3",
+        title: "San Martino in Badia, Italia",
+        rating: 5.0,
+        host: "Host: azienda specializzata",
+        price: 336,
+        date: "12 - 17 Apr",
+        images: [
+            "https://picsum.photos/id/36/300.jpg",
+            "https://picsum.photos/id/37/300.jpg",
+            "https://picsum.photos/id/38/300.jpg",
+        ],
+    },
+    {
+        id: "4",
+        title: "San Martino in Badia, Italia",
+        rating: 5.0,
+        host: "Host: azienda specializzata",
+        price: 336,
+        date: "12 - 17 Apr",
+        images: [
+            "https://picsum.photos/id/46/300.jpg",
+            "https://picsum.photos/id/47/300.jpg",
+            "https://picsum.photos/id/48/300.jpg",
+        ],
+    },
+    {
+        id: "5",
+        title: "San Martino in Badia, Italia",
+        rating: 5.0,
+        host: "Host: azienda specializzata",
+        price: 336,
+        date: "12 - 17 Apr",
+        images: [
+            "https://picsum.photos/id/56/300.jpg",
+            "https://picsum.photos/id/57/300.jpg",
+            "https://picsum.photos/id/58/300.jpg",
+        ],
+    },
+];
+
+const AirbnbCard = ({ id, title, rating, host, price, date, images }) => {
+    return (
+        <div className="relative text-sm">
+            <div className="flex justify-between items-center absolute top-3 left-3 right-3">
+                <span className="bg-white px-2 py-1 shadow-md border border-gray-100">Love by Guests</span>
+                <button className="p-3 bg-white rounded-full text-xl z-1">
+                    <IoHeartOutline />
+                </button>
+            </div>
+            <div className="flex overflow-x-hidden">
+                {images.map((image, index) => (
+                    <img className={classNames("rounded-xl", { "lazyload": index })} key={index} src={image} alt="" />
+                ))};
+            </div>
+            <div className="flex justify-between items-center gap-3 mt-2">
+                <h3 className="font-semibold">{title}</h3>
+                <div className="flex gap-1 items-center">
+                    <IoStar />
+                    <span>{rating}</span>
+                </div>
+            </div>
+            <p>{host}</p>
+            <p>{date}</p>
+            <p className="flex items-center gap-1">
+                <span className="font-semibold">{price} €</span>
+                <span>night</span>
+            </p>
+            <a href={`#more${id}`} className="block mt-2 bg-rose-400 text-white py-2 px-4 rounded-lg text-center before:absolute before:inset-0 before:rounded-xl before:content-['']">
+                View more
+            </a>
+        </div>
+    )
+}
+
 const Airbnb = () => {
 
     return (
@@ -80,8 +184,8 @@ const Airbnb = () => {
 
                 <div className="flex flex-col items-center gap-4">
                     <div className="flex gap-4">
-                        <button className="px-4 py-2 border rounded-full">Alloggi</button>
-                        <button className="px-4 py-2 border rounded-full">Esperienze</button>
+                        <button className="px-4 py-2 border rounded-lg">Alloggi</button>
+                        <button className="px-4 py-2 border rounded-lg">Esperienze</button>
                     </div>
 
                     <form className="flex rounded-full border border-gray-200 shadow-md text-xs flex-basis-0 items-center">
@@ -115,9 +219,10 @@ const Airbnb = () => {
                     </ul>
                 </nav>
             </section>
-            <section className="flex flex-col gap-4 mt-4">
+
+            <section className="flex flex-col gap-4 mt-8">
                 <div className="flex align-center gap-4 py-4 items-center">
-                    <nav className="overflow-x-auto">
+                    <nav className="overflow-x-hidden">
                         <ul className="flex items-center">
                             {navCategories.map((category, index) => (
                                 <li key={index} className="shrink-0">
@@ -129,19 +234,21 @@ const Airbnb = () => {
                             ))}
                         </ul>
                     </nav>
-                    <button className="flex items-center justify-center gap-2 px-4 py-2 bg-rose-400 text-white rounded-lg">
+                    <button className="flex items-center justify-center gap-2 px-4 py-2 border rounded-lg">
                         <IoOptions />
                         <span>Filter</span>
                     </button>
                 </div>
-                <div>
-                    <img src="https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b" alt="Airbnb" />
-                    <h3>San Martino in Badia, Italia</h3>
-                    <div>
-                        <IoStar />
-                        <span>5,0</span>
-                    </div>
+                <div className="mt-4 grid grid-cols-4 gap-6">
+                    {cards.map((card, index) => (
+                        <AirbnbCard key={index} {...card} />
+                    ))}
                 </div>
+                <div className="py-10 text-center">LOADING</div>
+            </section>
+
+            <section className="py-10">
+                nav3 & footer
             </section>
         </div>
     );
